cd50896156fceb45809b2639f238fc9503b29046
Roman Rayamajhi <163859106+RomanRayamajhii@users.noreply.github.com>
2026-04-26 13:27:41 +0545

    Delete 02routing directory

--- 02routing/app/products/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async  function Page({ params }: { params: { id: string }}) {
-    const { id } = await params;
-    return (
-        <div className="p-4 bg-gray-100 min-h-screen">
-            <h1 className='text-3xl font-bold text-green-700 text-center p-4'> This is product page with id: {id}</h1>
-        </div>
-    )
-}
