edcba5d116374025f211ee12683636d55b2e6796
RomanRayamajhii <romanraya4@gmail.com>
2026-04-26 14:40:29 +0545

    First project in next.js

---
+++ 01helloworld/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+const page = () => {
+  return (
+    <div className="p-4 bg-gray-100 min-h-screen items-center justify-center flex">
+      <h1 className='text-3xl font-bold text-green-700  p-4 '
+          
+        
+      >
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default page
